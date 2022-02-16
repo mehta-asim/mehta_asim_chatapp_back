c1f33600f272d649d6cdb3174620ac702b868082
mehta-asim <72264877+mehta-asim@users.noreply.github.com>
2022-02-15 20:36:50 -0500

    setup base io and socket and some event listeners for connection, message sent and user typing

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 import express from 'express';
+import {Server} from 'socket.io';
+import {createServer} from 'http';
 
 const app = express();
-
+const httpServer = createServer(app);
 const port = process.env.PORT || 3000;
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methos: ['GET','POST'],
+    },
+});
 
 app.get('/',(req, res) => {
     console.log("you connected");
     res.send("you connected");
 })
 
-app.listen(port, ()=>{
-    console.log(`back end up and running on ${port}`);
+
+httpServer.listen(port, ()=>{
+    console.log(`chatapp backend is running on ${port}`);
+})
+
+io.on('connection', (socket)=>{
+    
+    console.log('a user connected');
+
+    socket.emit('CONNECTED', socket.id);
+
+    socket.on('SEND_MESSAGE', (data) => {
+        
+        console.log("SEND_MESSAGE event!", data);
+
+        io.emit('MESSAGE', data);
+
+    })
+
+    socket.on('USER_TYPING', (data)=>{
+
+        io.emit('SOMEONE_TYPING', data);
+        
+    })
 })
