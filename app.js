12e160ce4474b2d02b6388a45452cc050ef48e31
mehta-asim <72264877+mehta-asim@users.noreply.github.com>
2022-03-05 21:25:58 -0500

    added all the remaining events and functions

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 const httpServer = createServer(app);
 const port = process.env.PORT || 3000;
 
+var usernames = [];
+
 
 const io = new Server(httpServer, {
     cors: {
@@ -30,19 +32,66 @@ io.on('connection', (socket)=>{
     
     console.log('a user connected');
 
+    io.emit('onconnect', usernames);
+
     socket.emit('CONNECTED', socket.id);
 
     socket.on('SEND_MESSAGE', (data) => {
         
-        console.log("SEND_MESSAGE event!", data);
+        // console.log("SEND_MESSAGE event!", data);
 
         io.emit('MESSAGE', data);
 
     })
 
+    socket.on('CONNECTED', (data) =>{
+        // console.log('CONNECTED event!', data);
+    })
+
     socket.on('USER_TYPING', (data)=>{
 
+        // console.log('USER_TYPING event!', data);
         io.emit('SOMEONE_TYPING', data);
 
     })
+
+    socket.on('USER_CONNECTED', (data)=>{
+        // console.log('USER_CONECTED event!', data);
+        socket.username = data.user;
+
+        usernames = [...usernames,data];
+
+        console.log(usernames);
+
+        io.emit('SOMEONE_CONNECTED', data);
+        // console.log('someone connected');
+
+    })
+
+    socket.on('EXITCHAT',(sock)=>{
+        // console.log('user disconnected 1st:',sock.username);
+        Array.prototype.forEach.call(usernames, (user,index) =>{
+            if(user.user === sock.username){
+                console.log('user disconnected :',sock.username);
+                usernames.splice(index,1);
+
+                io.emit('ondisconnect', usernames);
+            }
+        })
+    })
+
+    socket.on('disconnect', ()=>{
+
+        Array.prototype.forEach.call(usernames, (user,index) =>{
+            if(user.user === socket.username){
+                console.log('user diconnected:',socket.username);
+                usernames.splice(index,1);
+
+                io.emit('ondisconnect', usernames);
+            }
+        })
+
+        
+    })
+
 })
